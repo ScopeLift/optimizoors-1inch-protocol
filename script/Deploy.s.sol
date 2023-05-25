db6c6b17ea0b1cd7636b6c94873002f5de9a213a
Keating <keating.dev@protonmail.com>
2023-05-25 11:13:00 -0400

    Add benchmark and deploy script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.16;
+
+import {Script} from "forge-std/Script.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {OneInchContracts} from "test/OneInchContracts.sol";
+import {RouterFactory} from "src/RouterFactory.sol";
+
+contract Deploy is Script, OneInchContracts {
+  function run() public {
+    // Deploy the optimized router factory
+    vm.broadcast();
+    RouterFactory factory = new RouterFactory(
+            v5AggregationExecutor,
+            v5AggregationRouter,
+            v4AggregationExecutor,
+            v4AggregationRouter
+        );
+
+    // Deploy the optimized router for V5Aggregation
+    vm.broadcast();
+    factory.deploy(RouterFactory.RouterType.V5AggregationRouter, USDC);
+
+    // Deploy the optimized router for V4Aggregation
+    vm.broadcast();
+    factory.deploy(RouterFactory.RouterType.V4AggregationRouter, USDC);
+  }
+}
--- test/OneInchContracts.sol
+++ test/OneInchContracts.sol
@@ -18,6 +18,7 @@ contract OneInchContracts {
 
   address public immutable USDC = 0x7F5c764cBc14f9669B88837ca1490cCa17c31607;
   address public immutable UNI = 0x6fd9d7AD17242c41f7131d257212c54A0e816691;
+  address public immutable DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
 
   function returnSliceBytes(bytes calldata d) public pure returns (bytes memory) {
     return d[4:];

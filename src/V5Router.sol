cffba623489599d7e21c942789cc046bb72b517d
Keating <keating.dev@protonmail.com>
2023-06-06 15:17:35 -0400

    Add natspec for receive

--- src/V5Router.sol
+++ src/V5Router.sol
@@ -20,6 +20,10 @@ contract V5Router is AggregationV5BaseRouter {
     IERC20(token).approve(address(aggregationRouter), type(uint256).max);
   }
 
+  /// @dev If we remove this function solc will give a missing-receive-ether warning because we have
+  /// a payable fallback function. We cannot change the fallback function to a receive function
+  /// because receive does not have access to msg.data. In order to prevent a missing-receive-ether
+  /// warning we add a receive function and revert.
   receive() external payable {
     revert UnsupportedFunction();
   }
